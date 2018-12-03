f76176845e99b1bd4b57cc7de9d0f216d5859835
Joel Mackenzie <marihonjahmackenzie@gmail.com>
2018-12-03 09:48:50 +0300

    add h1

--- js/script.js
+++ js/script.js
@@ -3,10 +3,6 @@ var side = function(){
       var q = document.getElementById("one").value;
       var p = document.getElementById("two").value;
       var r = document.getElementById("three").value;
-<<<<<<< HEAD
-      
-=======
->>>>>>> 5a8e837d10973038153ef2bae21ef57deb7238f5
 
           if (q ===p && p === r && r === q) {
             alert("An Equilateral Triangle");
@@ -20,8 +16,5 @@ var side = function(){
         else if ((q+p)<=r || (q+r)<=p || (p+r)<=q) {
               alert("Not a triangle");
       }
-<<<<<<< HEAD
 
-=======
->>>>>>> 5a8e837d10973038153ef2bae21ef57deb7238f5
       };
